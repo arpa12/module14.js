004f9c353995c8e9011dd407fd0c70cd755b0e39
arpa12 <arpa15-12567@diu.edu.bd>
2022-02-01 21:26:33 +0600

    added this file

---
+++ inchtofeet.js
@@ -0,0 +1,15 @@
+var inch = 167;
+var feet = inch / 12;
+console.log(feet);
+
+//example//
+function inchToFeet(inch) {
+    var feet = inch / 12;
+    return feet;
+}
+//var senior=[145,903,234]
+//var nana=inchToFeet=(senior[0]);
+var nana = inchToFeet(156);
+console.log(nana);
+var nani = inchToFeet(277);
+console.log(nani);
