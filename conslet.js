ba78dca73e256f2c8d666d2204c16728fdca55bf
arpa12 <arpa15-12567@diu.edu.bd>
2022-02-01 23:45:34 +0600

    added const file

---
+++ conslet.js
@@ -0,0 +1,21 @@
+var name = "arpa nihan"
+if (name.length < 4) {
+    name = 'arpa';
+}
+console.log(name);
+
+//example
+var name = "arpa nihan"
+if (name.length > 4) {
+    name = 'arpa';
+}
+console.log(name);
+
+//const and let example//
+let age = 23; //let will use for changed value//
+age = 45;
+age = 90;
+console.log(age);
+
+const country = 'bangladesh'; //const will use for fixed value//
+console.log(country);
--- inchtofeet.js
+++ inchtofeet.js
@@ -8,7 +8,7 @@ function inchToFeet(inch) {
     return feet;
 }
 //var senior=[145,903,234]
-//var nana=inchToFeet=(senior[0]);
+//var nana=inchToFeet=(senior[0]); //0 means the position of upper array
 var nana = inchToFeet(156);
 console.log(nana);
 var nani = inchToFeet(277);
